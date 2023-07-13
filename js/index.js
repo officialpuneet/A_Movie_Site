d23cb2730b45f34f6a1ce8377127512299ef9beb
officialpuneet <67777174+officialpuneet@users.noreply.github.com>
2023-07-13 20:17:02 +0530

    Create index.js
    javascript for index

---
+++ js/index.js
@@ -0,0 +1,89 @@
+$(".c2").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoplay: true,
+  stagePadding: 40,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".fan-fav").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  margin: 30,
+  stagePadding: 40,
+  dots: false,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+    1200: {
+      items: 6,
+    },
+  },
+});
+$(".c3").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  autoplay: true,
+  dots: false,
+  stagePadding: 30,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+    1200: {
+      items: 6,
+    },
+  },
+});
+
+//explore
+
+$(".explore").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  autoplay: true,
+  stagePadding: 40,
+  dots: false,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+    1200: {
+      items: 6,
+    },
+  },
+});
